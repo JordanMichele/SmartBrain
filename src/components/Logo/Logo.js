cdf008869db10a17cb11fa801719a4f108afdf80
JordanMichele <jordanmichele32@gmail.com>
2018-10-30 10:31:52 -0400

    added Logo Component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,23 @@
+// Navigation Component
+import React from "react";
+import Tilt from "react-tilt";
+import GoBlue from "./michigan.png";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 50 }}
+        style={{ height: 250, width: 250 }}
+      >
+        <div className="Tilt-inner pa2">
+          <img style={{ paddingTop: "35px" }} src={GoBlue} />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
