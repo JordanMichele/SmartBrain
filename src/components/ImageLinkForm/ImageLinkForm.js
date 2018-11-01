4eaa762f751d24f7f983e95720e5df1767e03a62
Jordan Michele <jordanmichele@Jordanss-MacBook-Pro.local>
2018-11-01 14:24:38 -0400

    added input for ImageLinkForm

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,11 +5,12 @@ import React from "react";
 const ImageLinkForm = () => {
   return (
     <div>
-        <p className='f3'>
+        <p className='f4 pa2 w-70 center'>
             {'This Magic Brain will detect faces in your pictures. Give it a try '}
         </p>
         <div>
-            
+            <input className='f3' type='text' />
+            <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
         </div>
     </div>
   );
