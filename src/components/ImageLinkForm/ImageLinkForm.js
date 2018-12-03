ef6d65f1048932d5cd75abce1d922f994a0d8aa0
Jordan Michele <jordanmichele32@gmail.com>
2018-12-02 19:06:39 -0500

    built our components

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,5 +1,6 @@
 // Image Link Component
 import React from "react";
+import './ImageLinkForm.css';
 
 
 const ImageLinkForm = () => {
@@ -8,9 +9,11 @@ const ImageLinkForm = () => {
         <p className='f4 pa2 w-70 center'>
             {'This Magic Brain will detect faces in your pictures. Give it a try '}
         </p>
-        <div>
-            <input className='f3' type='text' />
-            <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+        <div className='center'>
+            <div className='form center pa4 br3 shadow-5'>
+                <input className='f4 pa2 w-70 center' type='text' />
+                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+            </div>     
         </div>
     </div>
   );
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+// Image Link Component
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+        <div className='white f3'>
+            {'Jordan, your current rank is...'}
+        </div>
+        <div className='white f1'>
+            {'#5'}
+        </div>
+    </div>
+  );
+};
+
+export default Rank;
